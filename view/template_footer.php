f810aa8c446f8b1e25b85060ef00292107703275
Linh204 <127870174+linhprogrammer@users.noreply.github.com>
2023-11-21 15:24:32 +0700

    Update template_footer.php

--- view/template_footer.php
+++ view/template_footer.php
@@ -1,14 +1,14 @@
 
         <!-- Footer Start -->
         <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
-            <div class="container py-5">
+        <div class="container py-5">
                 <div class="row g-5">
                     <div class="col-lg-3 col-md-6">
-                        <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Newsletter</h4>
-                        <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
+                        <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Newsletter</h4><br>
+                        <img src="img/logo1.png" width="150px" style="margin-bottom: 20px; margin-left: 20px; border-radius: 50%;" alt="">
                         <div class="position-relative mx-auto" style="max-width: 400px;">
-                            <input class="form-control border-primary w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
-                            <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
+                            <input class="form-control border-primary w-100 py-3 ps-4 pe-5" type="text" placeholder="Ý kiến của bạn">
+                            <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">Gửi</button>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6">
