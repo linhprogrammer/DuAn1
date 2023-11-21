d0d55e4ecb84376ff8b337f4a90a014e30fd9ebc
Linh204 <127870174+linhprogrammer@users.noreply.github.com>
2023-11-21 08:05:03 +0700

    Xoá đổi file

--- ttt/PIZZA/view/template_admin_footer.php
+++
@@ -1,5 +0,0 @@
-
-<script src="template/bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://kit.fontawesome.com/df103e3265.js" crossorigin="anonymous"></script>
-</body>
-</html>
