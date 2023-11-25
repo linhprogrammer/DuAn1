d6ceb8ad6d640261104e29c2a8387e23602ca0a6
Linh204 <127870174+linhprogrammer@users.noreply.github.com>
2023-11-25 07:54:42 +0700

    kk

--- view/page_info.php
+++ view/page_info.php
@@ -1,77 +1,83 @@
-<div class="container">
-        <h2>Thông tin tài khoản</h2>
-        <div class="account">
-            <div class="col-lg-3 col-md-3">
-                <div class="avatar">
-                    <img src="template/img/avatar.jpg" alt="">
-                    <input type="button" value="Thay ảnh"></input>
+<div class="info">
+    <div class="container">
+            <h2>Thông tin tài khoản</h2>
+            <div class="account">
+                <div class="col-lg-3 col-md-3">
+                    <div class="avatar">
+                        <img src="template/img/avatar.jpg" alt="">
+                        <input type="button" value="Thay ảnh"></input>
+                    </div>
+                </div>
+                <div class="col-lg-9 col-md-9">
+                    <div class="user">
+                        <form action="">
+                            <div class="hoten">
+                                <label>Họ và Tên:</label>
+                                <input type="text" value="<?= $data ['info']['hoten'] ?>" id="hoten">
+                            </div>
+                            <div class="sdt">
+                                <label>Số Điện Thoại:</label>
+                                <input type="text" value="<?= $data ['info']['sodienthoai'] ?>" id="sdt"></input>
+                            </div>
+                            <div class="email">
+                                <label>Email:</label>
+                                <input type="text" value="<?= $data ['info']['email'] ?>" id="email">
+                            </div>
+
+                            <div class="gioitinh">
+                                <label>Giới tính:</label>
+                                <div>
+                                    <input type="radio" id="radioNam" name="gioitinh" checked>
+                                    <label for="radioNam">Nam</label>
+                                </div>
+                            
+                                <div>
+                                    <input type="radio" id="radioNu" name="gioitinh">
+                                    <label for="radioNu">Nữ</label>
+                                </div>
+                            
+                                <div>
+                                    <input type="radio" id="radioKhac" name="gioitinh">
+                                    <label for="radioKhac">Khác</label>
+                                </div>
+                            </div>
+                            
+                            <div class="matkhau">
+                                <label>Mật Khẩu:</label>
+                                <input type="text" value="<?= $data ['info']['matkhau'] ?>" id="matkhau">
+                            </div>
+
+                            <div class="diachi">
+                                <label>Địa chỉ:</label>
+                                <input type="text" value="Quận 12, TP Hồ Chí Minh" id="diachi">
+                                <div>
+                                    <select id="city">
+                                        <option value="" selected>Chọn tỉnh thành</option>           
+                                    </select>
+                                            
+                                    <select id="district">
+                                        <option value="" selected>Chọn quận huyện</option>
+                                    </select>
+
+                                    <select id="ward">
+                                        <option value="" selected>Chọn phường xã</option>
+                                    </select>
+                                </div> 
+                            </div>
+                            <div>
+                                <a href=""><input type="button" value="Xem trạng thái đơn hàng"></input> </a>
+                            </div>
+                            <input type="submit" value="Lưu thay đổi">
+                        </form>
+                    </div>
                 </div>
             </div>
-            <div class="col-lg-9 col-md-9">
-                <div class="user">
-                    <form action="">
-                        <div class="hoten">
-                            <label>Họ và Tên:</label>
-                            <input type="text" value="<?= $data ['info']['hoten'] ?>" id="hoten">
-                        </div>
-                        <div class="sdt">
-                            <label>Số Điện Thoại:</label>
-                            <input type="text" value="<?= $data ['info']['sodienthoai'] ?>" id="sdt"></input>
-                        </div>
-                        <div class="email">
-                            <label>Email:</label>
-                            <input type="text" value="<?= $data ['info']['email'] ?>" id="email">
-                        </div>
-
-                        <div class="gioitinh">
-                            <label>Giới tính:</label>
-                            <div>
-                                <input type="radio" id="radioNam" name="gioitinh" checked>
-                                <label for="radioNam">Nam</label>
-                            </div>
-                        
-                            <div>
-                                <input type="radio" id="radioNu" name="gioitinh">
-                                <label for="radioNu">Nữ</label>
-                            </div>
-                        
-                            <div>
-                                <input type="radio" id="radioKhac" name="gioitinh">
-                                <label for="radioKhac">Khác</label>
-                            </div>
-                        </div>
-                        
-                        <div class="matkhau">
-                            <label>Mật Khẩu:</label>
-                            <input type="text" value="<?= $data ['info']['matkhau'] ?>" id="matkhau">
-                        </div>
-
-                        <div class="diachi">
-                            <label>Địa chỉ:</label>
-                            <input type="text" value="Quận 12, TP Hồ Chí Minh" id="diachi">
-                            <div>
-                                <select id="city">
-                                    <option value="" selected>Chọn tỉnh thành</option>           
-                                </select>
-                                        
-                                <select id="district">
-                                    <option value="" selected>Chọn quận huyện</option>
-                                </select>
-
-                                <select id="ward">
-                                    <option value="" selected>Chọn phường xã</option>
-                                </select>
-                            </div> 
-                        </div>
-                        <div>
-                            <a href=""><input type="button" value="Xem trạng thái đơn hàng"></input> </a>
-                        </div>
-                        <input type="submit" value="Lưu thay đổi">
-                    </form>
-                </div>
-            </div>
-        </div>
     </div>
+</div>
+
+
+
+
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js"></script>
