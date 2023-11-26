171f09483197a33a7b73db8dd07d6e9abf16acdb
NgocThien001 <149848336+NgocThien001@users.noreply.github.com>
2023-11-26 19:07:28 +0700

    update thông tin

--- controller/page.php
+++ controller/page.php
@@ -11,16 +11,97 @@ if (isset($_GET['act'])) {
             include_once 'view/page_home.php';
             include_once 'view/template_footer.php';
             break;
-            case 'info':
-                include_once 'model/connect.php';
-                include_once 'model/user.php';
-                if (isset($_GET['id'])) {
-                    $data['info'] = showinfo($_GET['id']);
+        case 'info':
+            include_once 'model/connect.php';
+            include_once 'model/user.php';
+
+            $data['info'] = []; // Initialize the variable
+
+            if (isset($_GET['id'])) {
+                $data['info'] = showinfo($_GET['id']);
+            }
+            if (isset($_POST['submit1'])) {
+                $matkhaucu = md5($_POST['matkhaucu']);
+                $matkhaumoi = md5($_POST['matkhaumoi']);
+                $matkhau = md5($_POST['matkhau']);
+                
+                if ($matkhaucu == $data['info']['matkhau']) {
+                    if ($matkhaumoi == $matkhau) {
+                        $kq=doimatkhau(
+                            $_GET['id'],
+                            $matkhau
+                        );
+                        if (empty($thongbao)) {
+                            echo '<p style="color: green;">Đã sửa thành công !!!!</p>';
+                        }
+                    } else {
+                        echo "Mật khẩu mới không khớp với mật khẩu xác nhận.";
+                    }
+                } else {
+                    echo "Mật khẩu cũ không đúng.";
                 }
-                include_once 'view/template_head.php';
-                include_once 'view/page_info.php';
-                include_once 'view/template_footer.php';
-                break;
+                    
+            }
+
+            if (isset($_POST['submit'])) {
+                // Get values from the form
+                $anh = $_FILES['hinhanh']['name'];
+            
+                // Check if $tinh, $huyen, $xa are empty or have default values
+                $tinh = isset($_POST['tinh']) && $_POST['tinh'] !== "" ? $_POST['tinh'] : $data['info']['tinh'];
+                $huyen = isset($_POST['huyen']) && $_POST['huyen'] !== "Chọn Quận/Huyện" ? $_POST['huyen'] : $data['info']['huyen'];
+                $xa = isset($_POST['xa']) && $_POST['xa'] !== "Chọn Phường/Xã" ? $_POST['xa'] : $data['info']['xa'];
+            
+                if ($_FILES['hinhanh']['error'] != 0) {
+                    $anh = $data['info']['hinhanh'];
+                }
+            
+                $kq = edit_info(
+                    $_GET['id'],
+                    $_POST['hoten'],
+                    $_POST['sodienthoai'],
+                    $_POST['email'],
+                    $_POST['gioitinh'],
+                    $anh,
+                    $tinh,
+                    $huyen,
+                    $xa
+                );
+            
+            
+            
+
+                if ($kq) {
+                    // If successful
+                    if ($_FILES['hinhanh']['error'] == 0) {
+                        $kq = move_uploaded_file(
+                            $_FILES['hinhanh']['tmp_name'],
+                            "upload/avatar/" . $_FILES['hinhanh']['name']
+                        );
+                    }
+
+                    // Check if there are no errors
+                    if (empty($thongbao)) {
+                        echo '<p style="color: green;">Đã sửa thành công !!!!</p>';
+                        echo '<script>
+                                setTimeout(function(){
+                                    window.location.href = "index.php?mod=page&act=info&id=' . $_GET['id'] . '";
+                                }, 1000); // Reload after 1 seconds
+                              </script>';
+                    }
+                    
+                    
+                    
+                }
+                
+            }
+            include_once 'view/template_head.php';
+            include_once 'view/page_info.php';
+            include_once 'view/template_footer.php';
+            break;
+
+
+
         default:
             #code...
             break;
--- view/page_info.php
+++ view/page_info.php
@@ -1,11 +1,12 @@
-<div class="info">
-    <div class="container">
+<form action="" method="post" enctype="multipart/form-data">
+    <div class="info">
+        <div class="container">
             <h2>Thông tin tài khoản</h2>
             <div class="account">
                 <div class="col-lg-3 col-md-3">
                     <div class="avatar">
-                        <img src="template/img/avatar.jpg" alt="">
-                        <input type="button" value="Thay ảnh"></input>
+                        <img src="upload/avatar/<?= $data['info']['hinhanh'] ?>" alt="" name="hinhanh">
+                        <input type="file" name="hinhanh" value="Thay ảnh"></input>
                     </div>
                 </div>
                 <div class="col-lg-9 col-md-9">
@@ -13,125 +14,254 @@
                         <form action="">
                             <div class="hoten">
                                 <label>Họ và Tên:</label>
-                                <input type="text" value="<?= $data ['info']['hoten'] ?>" id="hoten">
+                                <input type="text" value="<?= $data['info']['hoten'] ?>" name="hoten" id="hoten">
                             </div>
                             <div class="sdt">
                                 <label>Số Điện Thoại:</label>
-                                <input type="text" value="<?= $data ['info']['sodienthoai'] ?>" id="sdt"></input>
+                                <input type="text" value="<?= $data['info']['sodienthoai'] ?>" name="sodienthoai"
+                                    id="sdt"></input>
                             </div>
                             <div class="email">
                                 <label>Email:</label>
-                                <input type="text" value="<?= $data ['info']['email'] ?>" id="email">
+                                <input type="text" value="<?= $data['info']['email'] ?>" name="email" id="email">
                             </div>
-                            <div class="diachi">
-                                <label>Địa chỉ:</label>
-                                <div>
-                                    <select id="city">
-                                        <option value="" selected>Chọn tỉnh thành</option>           
-                                    </select>
-                                            
-                                    <select id="district">
-                                        <option value="" selected>Chọn quận huyện</option>
-                                    </select>
+                            <fieldset class="address-frame">
+                                <legend>Địa chỉ</legend>
 
-                                    <select id="ward">
-                                        <option value="" selected>Chọn phường xã</option>
-                                    </select>
-                                </div> 
+                                <div class="diachi">
+                                    <label>Địa chỉ:</label>
+
+                                    <div>
+                                        <span>Tỉnh: </span>
+                                        <span>
+                                            <?php echo !empty($data['info']['tinh']) ? $data['info']['tinh'] : 'Bạn chưa chọn tỉnh'; ?>
+                                        </span> <br>
+
+                                        <span>Quận/Huyện: </span>
+                                        <span>
+                                            <?php echo !empty($data['info']['huyen']) ? $data['info']['huyen'] : 'Bạn chưa chọn huyện'; ?>
+                                        </span> <br>
+
+                                        <span>Phường/Xã: </span>
+                                        <span>
+                                            <?php echo !empty($data['info']['xa']) ? $data['info']['xa'] : 'Bạn chưa chọn xã'; ?>
+                                        </span> <br>
+                                    </div>
+
+                                    <!-- Add a button to trigger the address editing section -->
+                                    <input type="button" value="Thay đổi địa chỉ" name="thaydoidiachi"
+                                        onclick="toggleAddressSection()">
+
+                                    <!-- Address editing section initially hidden -->
+                                    <div id="diachiSection" style="display: none;">
+                                        <label>Địa chỉ:</label>
+
+                                        <div>
+                                            <select id="city" name="tinh">
+                                                <option value="Chọn Tỉnh" selected>
+                                                    <?= $data['info']['tinh'] ?>
+                                                </option>
+                                                <!-- Add other options as needed -->
+                                            </select>
+
+                                            <select id="district" name="huyen">
+                                                <option value="Chọn Quận/Huyện" selected>
+                                                    <?= $data['info']['huyen'] ?>
+                                                </option>
+                                                <!-- Add other options as needed -->
+                                            </select>
+
+                                            <select id="ward" name="xa">
+                                                <option value="Chọn Phường/Xã" selected>
+                                                    <?= $data['info']['xa'] ?>
+                                                </option>
+                                                <!-- Add other options as needed -->
+                                            </select>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </fieldset>
+
+
+
+
+
+
+
+                            <div class="password-change-frame">
+                                <div class="matkhau">
+                                    <label>Mật Khẩu:</label>
+                                    <input type="button" value="Thay đổi mật khẩu" id="showForm">
+                                </div>
+
+                                <div id="passwordChangeForm" style="display: none;">
+                                    <label>Mật khẩu cũ:</label>
+                                    <input type="password" id="oldPassword" name="matkhaucu"> <br>
+
+                                    <label>Mật khẩu mới:</label>
+                                    <input type="password" id="newPassword" name="matkhaumoi"> <br>
+
+                                    <label>Xác nhận mật khẩu mới:</label>
+                                    <input type="password" id="confirmPassword" name="matkhau"> <br>
+
+                                    <input type="submit" value="Lưu thay đổi" name="submit1"
+                                        onclick="savePasswordChanges()">
+                                </div>
                             </div>
+
                             <div class="gioitinh">
                                 <label>Giới tính:</label>
                                 <div>
-                                    <input type="radio" id="radioNam" name="gioitinh" checked>
-                                    <label for="radioNam">Nam</label>
-                                </div>
-                            
-                                <div>
-                                    <input type="radio" id="radioNu" name="gioitinh">
-                                    <label for="radioNu">Nữ</label>
-                                </div>
-                            
-                                <div>
-                                    <input type="radio" id="radioKhac" name="gioitinh">
-                                    <label for="radioKhac">Khác</label>
+                                    <select id="gioitinh" name="gioitinh">
+                                        <option value="Nam" <?= ($data['info']['gioitinh'] == 'Nam') ? 'selected' : '' ?>>
+                                            Nam</option>
+                                        <option value="Nữ" <?= ($data['info']['gioitinh'] == 'Nữ') ? 'selected' : '' ?>>Nữ
+                                        </option>
+                                        <option value="Khác" <?= ($data['info']['gioitinh'] == 'Khác') ? 'selected' : '' ?>>Khác</option>
+                                    </select>
                                 </div>
                             </div>
-                            
-                            <div class="matkhau">
-                                <label>Mật Khẩu:</label>
-                                <input type="button" value="Thay đổi mật khẩu" id="showForm">
-                            </div>
-
                             <div>
-                                <a href=""><input type="button" value="Xem trạng thái đơn hàng"></input> </a>
-                                <input type="submit" value="Lưu thay đổi">
+                                <?php if (isset($thongbao)): ?>
+                                    <div class="alert alert-warning">
+                                        <?= $thongbao ?>
+                                    </div>
+                                <?php endif;
+                                unset($thongbao); ?>
+                                <a href=""><input type="button" value="Xem trạng thái đơn hàng"></input></a>
+                                <input type="submit" value="Lưu thay đổi" name="submit">
                             </div>
 
-                    </form>
+
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</form>
+<a href="index.php?mod=user&act=home&id=<?= $data ['info']['matk'] ?>"><input type="button" value="Về trang chủ"></a>
 
 
 
-
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js"></script>
-    <script>
-        const host = "https://provinces.open-api.vn/api/";
-        var callAPI = (api) => {
-            return axios.get(api)
-                .then((response) => {
-                    renderData(response.data, "city");
-                });
-        }
-        callAPI('https://provinces.open-api.vn/api/?depth=1');
-        var callApiDistrict = (api) => {
-            return axios.get(api)
-                .then((response) => {
-                    renderData(response.data.districts, "district");
-                });
-        }
-        var callApiWard = (api) => {
-            return axios.get(api)
-                .then((response) => {
-                    renderData(response.data.wards, "ward");
-                });
-        }
-
-        var renderData = (array, select) => {
-            let row = ' <option disable value="">Chọn</option>';
-            array.forEach(element => {
-                row += `<option data-id="${element.code}" value="${element.name}">${element.name}</option>`
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js"></script>
+<script>
+    const host = "https://provinces.open-api.vn/api/";
+    var callAPI = (api) => {
+        return axios.get(api)
+            .then((response) => {
+                renderData(response.data, "city");
             });
-            document.querySelector("#" + select).innerHTML = row
+    }
+    callAPI('https://provinces.open-api.vn/api/?depth=1');
+    var callApiDistrict = (api) => {
+        return axios.get(api)
+            .then((response) => {
+                renderData(response.data.districts, "district");
+            });
+    }
+    var callApiWard = (api) => {
+        return axios.get(api)
+            .then((response) => {
+                renderData(response.data.wards, "ward");
+            });
+    }
+
+    var renderData = (array, select) => {
+        let row = ' <option disable value="">Chọn</option>';
+        array.forEach(element => {
+            row += `<option data-id="${element.code}" value="${element.name}">${element.name}</option>`
+        });
+        document.querySelector("#" + select).innerHTML = row
+    }
+
+    $("#city").change(() => {
+        callApiDistrict(host + "p/" + $("#city").find(':selected').data('id') + "?depth=2");
+        printResult();
+    });
+    $("#district").change(() => {
+        callApiWard(host + "d/" + $("#district").find(':selected').data('id') + "?depth=2");
+        printResult();
+    });
+    $("#ward").change(() => {
+        printResult();
+    })
+
+    var printResult = () => {
+        if ($("#district").find(':selected').data('id') != "" && $("#city").find(':selected').data('id') != "" &&
+            $("#ward").find(':selected').data('id') != "") {
+            let result = $("#city option:selected").text() +
+                " | " + $("#district option:selected").text() + " | " +
+                $("#ward option:selected").text();
+            $("#result").text(result)
         }
 
-        $("#city").change(() => {
-            callApiDistrict(host + "p/" + $("#city").find(':selected').data('id') + "?depth=2");
-            printResult();
-        });
-        $("#district").change(() => {
-            callApiWard(host + "d/" + $("#district").find(':selected').data('id') + "?depth=2");
-            printResult();
-        });
-        $("#ward").change(() => {
-            printResult();
-        })
+    }
 
-        var printResult = () => {
-            if ($("#district").find(':selected').data('id') != "" && $("#city").find(':selected').data('id') != "" &&
-                $("#ward").find(':selected').data('id') != "") {
-                let result = $("#city option:selected").text() +
-                    " | " + $("#district option:selected").text() + " | " +
-                    $("#ward option:selected").text();
-                $("#result").text(result)
-            }
+</script>
+<script>
+    document.getElementById("showForm").addEventListener("click", function () {
+        // Toggle the visibility of the password change form
+        var form = document.getElementById("passwordChangeForm");
+        form.style.display = (form.style.display === "none") ? "block" : "none";
+    });
 
-        }
+    function savePasswordChanges() {
+        // Add your logic to save password changes here
+        var oldPassword = document.getElementById("oldPassword").value;
+        var newPassword = document.getElementById("newPassword").value;
+        var confirmPassword = document.getElementById("confirmPassword").value;
 
-	</script>
-    
+        // Add logic to handle password change (e.g., using AJAX to send data to the server)
+        console.log("Old Password: " + oldPassword);
+        console.log("New Password: " + newPassword);
+        console.log("Confirm Password: " + confirmPassword);
+
+        // You can add AJAX or form submission logic here
+    }
+
+</script>
+<script>
+    function toggleAddressSection() {
+        var addressSection = document.getElementById('diachiSection');
+        addressSection.style.display = (addressSection.style.display === 'none') ? 'block' : 'none';
+    }
+
+
+</script>
+<style>
+    .address-frame {
+        border: 1px solid #ccc;
+        padding: 10px;
+        margin-bottom: 10px;
+    }
+
+    .password-change-frame {
+        border: 1px solid #ccc;
+        padding: 10px;
+        margin: 10px;
+        width: 300px;
+    }
+
+    .matkhau,
+    #passwordChangeForm {
+        margin-bottom: 10px;
+    }
+
+    label {
+        display: block;
+        margin-bottom: 5px;
+    }
+
+    input {
+        width: 100%;
+        padding: 5px;
+        margin-bottom: 10px;
+    }
+
+    #passwordChangeForm {
+        display: none;
+    }
+</style>
