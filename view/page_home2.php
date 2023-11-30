5ec06c85b793e29d1082d9e39d3b9e672f067b00
Linh204 <127870174+linhprogrammer@users.noreply.github.com>
2023-11-30 09:03:08 +0700

    da sua

--- view/page_home2.php
+++ view/page_home2.php
@@ -54,9 +54,9 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <div class="navbar-nav ms-auto py-0 pe-4">
-                        <a href="index.html" class="nav-item nav-link active">Trang Chủ</a>
+                        <a href="index.php?mod=page&act=home" class="nav-item nav-link active">Trang Chủ</a>
                         <div class="nav-item dropdown">
-                            <a href="menu.html" class="nav-item nav-link">Thực Đơn</a>
+                            <a href="index.php?mod=page&act=menu" class="nav-item nav-link">Thực Đơn</a>
                             <div class="dropdown-menu m-0">
                                 <a href="#" class="dropdown-item">Gà Rán</a>
                                 <a href="#" class="dropdown-item">Mì Ý</a>
