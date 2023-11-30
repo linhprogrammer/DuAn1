30537073c1852216ba2336d403bb0fccc2a618f2
Linh204 <127870174+linhprogrammer@users.noreply.github.com>
2023-11-30 08:14:29 +0700

    da sua

--- view/page_search.php
+++ view/page_search.php
@@ -1,17 +1,23 @@
 <div class="row g-4">
     <?php foreach ($data['dssp'] as $sp): ?>
-        <div class="col-lg-6">
-            <div class="d-flex align-items-center">
-                <a href="index.php?mod=product&act=detail&id=<?= $sp['masp'] ?>" style="text-decoration: none;">
-                    <img class="flex-shrink-0 img-fluid rounded" src="upload/product/<?= $sp['hinhanhshow'] ?>" alt="" style="width: 150px;">
-                    <div class="w-100 d-flex flex-column text-start ps-4">
-                        <h5 class="d-flex justify-content-between border-bottom pb-2">
-                            <span class="text-danger"><?= $sp['tensp'] ?></span>
-                            <span class="text-danger"><?= $sp['giakm'] ?> VNĐ</span>
-                        </h5>
-                        <small class="fst-italic"><?= $sp['motangan'] ?></small>
-                    </div>
-                </a>
+        <div class="col-lg-3">
+            <div class="card">
+                <div class="align-items-center">
+                    <a href="index.php?mod=product&act=detail&id=<?= $sp['masp'] ?>" style="text-decoration: none;">
+                        <img class="flex-shrink-0 img-fluid rounded" src="upload/product/<?= $sp['hinhanhshow'] ?>" alt="" style="width: 310px; margin: auto; margin-top: 20px;">
+                        <div class="w-100 text-start ps-4">
+                            <h5 class=" justify-content-between border-bottom pb-2">
+                                <p style=" color: #C5221F;margin: auto; text-align: center;"><?= $sp['tensp'] ?></p>
+                                <p style=" color: #C5221F; font-size: 16px; margin: auto; text-align: center;"><?= $sp['motangan'] ?></p>           
+                            </h5>
+                            <p style="font-size: 18px; margin: auto; text-align: center; " class="text-danger"><?= $sp['giakm'] ?> VNĐ</p>
+                            <div class="nut-them">
+                                <a href="#" class="btn" style="background-color: #C5221F; border-radius: 100px; margin-bottom: 10px; color: white;  width: 90%;">Thêm vào giỏ hàng</a>
+                                <a href="#" class="btn" style="background-color: #C5221F; border-radius: 100px; color: white; width: 90%; margin-bottom: 10px;">Mua ngay</a>
+                            </div>
+                        </div>
+                    </a>
+                </div>
             </div>
         </div>
     <?php endforeach; ?>
