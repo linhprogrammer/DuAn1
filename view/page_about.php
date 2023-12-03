47a3191413928b9c4c4c6337ea9a38989587875a
Linh204 <127870174+linhprogrammer@users.noreply.github.com>
2023-12-03 10:02:17 +0700

    Update page_about.php

--- view/page_about.php
+++ view/page_about.php
@@ -104,47 +104,38 @@
             <div class="container">
                 <div class="row g-5 align-items-center">
                     <div class="col-lg-6">
-                        <div class="row g-3">
-                            <div class="col-6 text-start">
+                        <div class="row g-6">
+                            <div class="col-8" style="margin-left: 60px;">
                                 <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.1s" src="template/img/about-1.jpg">
-                            </div>
-                            <div class="col-6 text-start">
-                                <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.3s" src="template/img/about-2.jpg" style="margin-top: 25%;">
-                            </div>
-                            <div class="col-6 text-end">
-                                <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.5s" src="template/img/about-3.jpg">
-                            </div>
-                            <div class="col-6 text-end">
-                                <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.7s" src="template/img/about-4.jpg">
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-6">
-                        <h5 class="section-title ff-secondary text-start text-primary fw-normal">Về chúng tôi</h5>
-                        <h1 class="mb-4">Chào mừng đến với <i class="fa fa-utensils text-primary me-2"></i>Xì Food</h1>
+                        <h5 class="section-title ff-secondary text-start text-danger fw-normal">Về Chúng Tôi</h5>
+                        <h1 class="mb-4">Chào Mừng Đến Với <i class="fa fa-utensils text-danger me-2"></i>XÌ Food</h1>
                         <p class="mb-4">Chúng tôi là những người trẻ đam mê với việc nấu nướng và muốn mang lại cho mọi người những món ăn ngon tiện lợi.</p>
                         <p class="mb-4">Sự hài lòng của khách hàng là niềm vui của chung tôi.</p>
                         <div class="row g-4 mb-4">
                             <div class="col-sm-6">
-                                <div class="d-flex align-items-center border-start border-5 border-primary px-3">
-                                    <h1 class="flex-shrink-0 display-5 text-primary mb-0" data-toggle="counter-up">5</h1>
+                                <div class="d-flex align-items-center border-start border-5 border-danger px-3">
+                                    <h1 class="flex-shrink-0 display-5 text-danger mb-0" data-toggle="counter-up">5</h1>
                                     <div class="ps-4">
                                         <p class="mb-0">Năm</p>
-                                        <h6 class="text-uppercase mb-0">Kinh nghiệm</h6>
+                                        <h6 class="text-uppercase mb-0">Kinh Nghiệm</h6>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-sm-6">
-                                <div class="d-flex align-items-center border-start border-5 border-primary px-3">
-                                    <h1 class="flex-shrink-0 display-5 text-primary mb-0" data-toggle="counter-up">70</h1>
+                                <div class="d-flex align-items-center border-start border-5 border-danger px-3">
+                                    <h1 class="flex-shrink-0 display-5 text-danger mb-0" data-toggle="counter-up">70</h1>
                                     <div class="ps-4">
-                                        <p class="mb-0">Nhân viên</p>
-                                        <h6 class="text-uppercase mb-0">Chuyên nghiệp </h6>
+                                        <p class="mb-0">Nhân Viên</p>
+                                        <h6 class="text-uppercase mb-0">Chuyên Nghiệp</h6>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <a class="btn btn-primary py-3 px-5 mt-2" href="">Xem thêm</a>
+                        <a class="btn btn-danger py-3 px-5 mt-2" href="">Xem Thêm</a>
                     </div>
                 </div>
             </div>
@@ -156,7 +147,7 @@
         <div class="container-xxl pt-5 pb-3">
             <div class="container">
                 <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                    <h5 class="section-title ff-secondary text-center text-primary fw-normal">Thành viên của Xì Food</h5>
+                    <h5 class="section-title ff-secondary text-center text-danger fw-normal">Thành viên của Xì Food</h5>
                     <h1 class="mb-5">Đầu bếp bậc thầy của chúng tôi</h1>
                 </div>
                 <div class="row g-4">
@@ -168,9 +159,9 @@
                             <h5 class="mb-0">Full Name</h5>
                             <small>Designation</small>
                             <div class="d-flex justify-content-center mt-3">
-                                <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-instagram"></i></a>
+                                <a class="btn btn-square btn-danger mx-1" href=""><i class="fab fa-facebook-f"></i></a>
+                                <a class="btn btn-square btn-danger mx-1" href=""><i class="fab fa-twitter"></i></a>
+                                <a class="btn btn-square btn-danger mx-1" href=""><i class="fab fa-instagram"></i></a>
                             </div>
                         </div>
                     </div>
@@ -182,9 +173,9 @@
                             <h5 class="mb-0">Full Name</h5>
                             <small>Designation</small>
                             <div class="d-flex justify-content-center mt-3">
-                                <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-instagram"></i></a>
+                                <a class="btn btn-square btn-danger mx-1" href=""><i class="fab fa-facebook-f"></i></a>
+                                <a class="btn btn-square btn-danger mx-1" href=""><i class="fab fa-twitter"></i></a>
+                                <a class="btn btn-square btn-danger mx-1" href=""><i class="fab fa-instagram"></i></a>
                             </div>
                         </div>
                     </div>
@@ -196,9 +187,9 @@
                             <h5 class="mb-0">Full Name</h5>
                             <small>Designation</small>
                             <div class="d-flex justify-content-center mt-3">
-                                <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-instagram"></i></a>
+                                <a class="btn btn-square btn-danger mx-1" href=""><i class="fab fa-facebook-f"></i></a>
+                                <a class="btn btn-square btn-danger mx-1" href=""><i class="fab fa-twitter"></i></a>
+                                <a class="btn btn-square btn-danger mx-1" href=""><i class="fab fa-instagram"></i></a>
                             </div>
                         </div>
                     </div>
@@ -210,9 +201,9 @@
                             <h5 class="mb-0">Full Name</h5>
                             <small>Designation</small>
                             <div class="d-flex justify-content-center mt-3">
-                                <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square btn-primary mx-1" href=""><i class="fab fa-instagram"></i></a>
+                                <a class="btn btn-square btn-danger mx-1" href=""><i class="fab fa-facebook-f"></i></a>
+                                <a class="btn btn-square btn-danger mx-1" href=""><i class="fab fa-twitter"></i></a>
+                                <a class="btn btn-square btn-danger mx-1" href=""><i class="fab fa-instagram"></i></a>
                             </div>
                         </div>
                     </div>
