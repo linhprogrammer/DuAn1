82d82e526bc42dbb6010a0884c1bde08703ae495
Linh204 <127870174+linhprogrammer@users.noreply.github.com>
2023-12-03 09:30:35 +0700

    Update template_footer.php

--- view/template_footer.php
+++ view/template_footer.php
@@ -4,8 +4,7 @@
         <div class="container py-5">
                 <div class="row g-5">
                     <div class="col-lg-3 col-md-6">
-                        <h4 class="section-title ff-secondary text-start text-danger fw-normal mb-4">Newsletter</h4><br>
-                        <img src="img/logo1.png" width="150px" style="margin-bottom: 20px; margin-left: 20px; border-radius: 50%;" alt="">
+                        <img src="upload/logo.png" width="150px" style="margin-bottom: 20px; margin-left: 20px; border-radius: 50%;" alt="">
                         <div class="position-relative mx-auto" style="max-width: 400px;">
                             <input class="form-control border-danger w-100 py-3 ps-4 pe-5" type="text" placeholder="Ý kiến của bạn">
                             <button type="button" class="btn btn-danger py-2 position-absolute top-0 end-0 mt-2 me-2">Gửi</button>
@@ -38,19 +37,8 @@
             <div class="container">
                 <div class="copyright">
                     <div class="row">
-                        <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                            &copy; <a class="border-bottom" href="#">Your Site Name</a>, All Right Reserved.
-
-                            <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                            Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a>
-                        </div>
-                        <div class="col-md-6 text-center text-md-end">
-                            <div class="footer-menu">
-                                <a href="">Home</a>
-                                <a href="">Cookies</a>
-                                <a href="">Help</a>
-                                <a href="">FQAs</a>
-                            </div>
+                        <div class="col-md-12 text-center text-md-center mb-3 mb-md-0">
+                            &copy; <a class="border-bottom" href="#">Xì Food</a>, All Right Reserved.
                         </div>
                     </div>
                 </div>
