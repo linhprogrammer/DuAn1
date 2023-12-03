9dcbc9f24c5212426ab36d490d530d5b27e390fa
Linh204 <127870174+linhprogrammer@users.noreply.github.com>
2023-12-03 09:30:37 +0700

    Update page_home.php

--- view/page_home.php
+++ view/page_home.php
@@ -415,18 +415,9 @@
             <div class="container">
                 <div class="row g-5 align-items-center">
                     <div class="col-lg-6">
-                        <div class="row g-3">
-                            <div class="col-6 text-start">
+                        <div class="row g-6">
+                            <div class="col-8" style="margin-left: 60px;">
                                 <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.1s" src="template/img/about-1.jpg">
-                            </div>
-                            <div class="col-6 text-start">
-                                <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.3s" src="template/img/about-2.jpg" style="margin-top: 25%;">
-                            </div>
-                            <div class="col-6 text-end">
-                                <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.5s" src="template/img/about-3.jpg">
-                            </div>
-                            <div class="col-6 text-end">
-                                <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.7s" src="template/img/about-4.jpg">
                             </div>
                         </div>
                     </div>
